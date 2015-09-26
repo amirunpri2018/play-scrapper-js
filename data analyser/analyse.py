4a95574c524f047b8a2de0534d0b0108360be621
Divam Gupta <divamgupta@gmail.com>
2015-09-26 17:47:34 +0530

    1

--- data analyser/analyse.py
+++ data analyser/analyse.py
@@ -1,1 +1,17 @@
 # aa
+import json
+from pprint import pprint
+
+
+jsonFileFolder = "./play scrapper /datajson"
+int maxJSONFiles = 100
+
+def loadIthJSONFile(i):
+	with open('data.json') as data_file:
+		data = json.load(data_file)
+		return data
+
+
+
+for i in range(maxJSONFiles):
+	pprint ( loadIthJSONFile(i)  )
