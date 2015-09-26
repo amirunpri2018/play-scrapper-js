8320a9544c499dd7675438e99ae7f1fbf557229a
Divam Gupta <divamgupta@gmail.com>
2015-09-26 22:18:48 +0530

    fileloader made

--- data analyser/analyse.py
+++ data analyser/analyse.py
@@ -3,15 +3,56 @@ import json
 from pprint import pprint
 
 
-jsonFileFolder = "../play scrapper/datajson/"
-maxJSONFiles = 100
 
-def loadIthJSONFile(i):
-	with open(jsonFileFolder + 'data'+str(i)+'.json') as data_file:
-		data = json.load(data_file)
-		return data
+class dataLoader:
+	
+	jsonFileFolder = "../play scrapper /datajson/"
+	maxJSONFiles = 1
+
+	jsonFileIndex = -1
+	arrayElementIndex = 0
+
+	lastJsonRead = []
+
+	def loadIthJSONFile(self , i):
+		with open(self.jsonFileFolder + 'datai-'+str(i)+'.json') as data_file:
+			data = json.load(data_file)
+			self.lastJsonRead = data
+			return data
+
+	def getNext(self):
+		if self.arrayElementIndex >= len(self.lastJsonRead) and self.jsonFileIndex >= self.maxJSONFiles:
+			return None
+
+		if self.arrayElementIndex >= len(self.lastJsonRead):
+			self.jsonFileIndex += 1
+			self.loadIthJSONFile(self.jsonFileIndex)
+			self.arrayElementIndex = 0
+
+		
+		r =  self.lastJsonRead[self.arrayElementIndex]
+		self.arrayElementIndex += 1
+		return r
+
+	def hasNext(self):
+		if self.arrayElementIndex >= len(self.lastJsonRead) and self.jsonFileIndex >= self.maxJSONFiles:
+			return False
+		else:
+			return True
+
+
+d = dataLoader()
+
+# d.getNext()   
+# d.getNext()   
+# d.getNext() 
+# pprint (   d.getNext()   )
+
+while d.hasNext() :
+	
+	pprint ( d.getNext()['PackegeId'] )
 
 
 
-for i in range(maxJSONFiles):
-	pprint ( loadIthJSONFile(i)  )
+
+
